9ded406d4f54eaa7196bc4eccec6faeb374e8fb2
Jose Betancourt <josebp_25@hotmail.com>
2021-05-02 17:00:09 -0400

    first commit ar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzistorescss';
+
+  items = ['hola', 'chao', 'que tal']
+
+    products: Product[] = [
+      {
+        id: '1',
+        image: 'assets/camisa.png',
+        title: 'Camisa',
+        price: 40000,
+        description: 'bla bla bla'
+      },
+      {
+        id: '2',
+        image: 'assets/hoodie.png',
+        title: 'Hoodie',
+        price: 40000,
+        description: 'bla bla bla'
+      },
+      {
+        id: '3',
+        image: 'assets/chaquetatraje.png',
+        title: 'Traje',
+        price: 40000,
+        description: 'bla bla bla'
+      },
+      {
+        id: '4',
+        image: 'assets/chemisenegra.png',
+        title: 'Chemise',
+        price: 40000,
+        description: 'bla bla bla'
+      },
+      {
+        id: '5',
+        image:  'assets/tshirt.png',
+        title: 'T shirt',
+        price: 40000,
+        description: 'bla bla bla'
+      },
+    ]
+
+  addItem(){
+    this.items.push('Nuevo saludo')
+  }
+
+  deleteItem(index: number){
+    this.items.splice(index, 1)
+
+
+  }
 }
